03906500ce30f7573c1006c1c4ab53e4dbc1f6e8
thetimverse <tim.mockingjay09@gmail.com>
2024-03-12 15:59:36 +0100

    feat: queries categories

--- src/views/CategoryPage.tsx
+++ src/views/CategoryPage.tsx
@@ -1,10 +1,13 @@
 import {useParams} from "react-router-dom";
+import {useGetCategoryQuery} from "../entities/products/queries.ts";
 
 function CategoryPage() {
     const {id} = useParams();
+    const {data} = useGetCategoryQuery(Number(id));
+    console.log(data?.data);
 
     return (
-        <div>{id}</div>
+        <div><h2>{data?.data.attributes.name}</h2></div>
     );
 }
 
